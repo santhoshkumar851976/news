5cf4f6a42855f5c1c86540c909d9aed1472fae94
santhoshkumar851976 <159409614+santhoshkumar851976@users.noreply.github.com>
2024-03-12 14:44:49 +0530

    Update and rename c.c to c.py

--- c.c
+++
@@ -1,8 +0,0 @@
-for (i = 0; i < n; ++i){
-   for 
-      if (){
-         a = num[];
-         num[] = num[j];
-         num[j] = ;
-      }
-   }
---
+++ c.py
@@ -0,0 +1,5 @@
+def divide_by_zero(a, b):
+    return a / b
+
+result = divide_by_zero(10, 0)
+print("Result:", result)
